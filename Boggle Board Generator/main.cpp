5fb0369ac3245c32aba34543776b3a9b16164179
mudit182 <mudit182@gmail.com>
2016-12-27 16:25:59 -0600

    Initial Commit

---
+++ Boggle Board Generator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Boggle Board Generator
+//
+//  Created by Mudit Gurjar on 12/27/16.
+//  Copyright © 2016 Mudit Gurjar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
